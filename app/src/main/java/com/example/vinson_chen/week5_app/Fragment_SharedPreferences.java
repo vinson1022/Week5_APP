f9b67a684c6c64d1215beefedb87491c106975d3
vinson1022 <vinson510123@gmail.com>
2015-12-16 11:20:56 +0800

    [UPDATE] UI finish

---
+++ app/src/main/java/com/example/vinson_chen/week5_app/Fragment_SharedPreferences.java
@@ -0,0 +1,21 @@
+package com.example.vinson_chen.week5_app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vinson_Chen on 2015/12/16.
+ */
+public class Fragment_SharedPreferences extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.layout_fragment_sp, container, false);
+    }
+
+}
--- app/src/main/java/com/example/vinson_chen/week5_app/MainActivity.java
+++ app/src/main/java/com/example/vinson_chen/week5_app/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.vinson_chen.week5_app;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button_fragment_sp,button_fragment_is,button_fragment_sql;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setView();
+        setListener();
+
+    }
+
+    private void setListener() {
+
+        button_fragment_sp.setOnClickListener(this);
+        button_fragment_is.setOnClickListener(this);
+        button_fragment_sql.setOnClickListener(this);
+
+    }
+
+    private void setView() {
+
+        button_fragment_sp = (Button)findViewById(R.id.button_sp);
+        button_fragment_is = (Button)findViewById(R.id.button_is);
+        button_fragment_sql = (Button)findViewById(R.id.button_sql);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("Vinson","");
+        if(v.getId() == button_fragment_sp.getId()){
+            Fragment_SharedPreferences fragment = new Fragment_SharedPreferences();
+            getFragmentManager().beginTransaction().replace(R.id.fragment_content, fragment).commit();
+        }else if(v.getId() == button_fragment_is.getId()){
+            Fragment_InternalStorage fragment = new Fragment_InternalStorage();
+            getFragmentManager().beginTransaction().replace(R.id.fragment_content, fragment).commit();
+        }else if(v.getId() == button_fragment_sql.getId()){
+            Fragment_SQL fragment = new Fragment_SQL();
+            getFragmentManager().beginTransaction().replace(R.id.fragment_content, fragment).commit();
+        }
     }
 }
